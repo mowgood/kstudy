08e839cb3a2160f4f08ae66dac97650cdd9c84b7
mowgood <99533453+mowgood@users.noreply.github.com>
2023-03-24 10:01:35 +0900

    JDBC 실습

--- JAVA/javaedu/src/day14/exercise/SelectEmpLab.java
+++ JAVA/javaedu/src/day14/exercise/SelectEmpLab.java
@@ -11,44 +11,38 @@ public class SelectEmpLab {
 		String url = "jdbc:mysql://localhost:3306/edudb?characterEncoding=UTF-8&serverTimezone=UTC";
 		String user = "jdbctest";
 		String passwd = "jdbctest";
-		
+		String sql = "";
+
 		boolean random = new Random().nextBoolean();
-		
+
 		System.out.println("random = " + random);
-		
-		if(random) { //true
-			String sql = "SELECT ename, format(sal,0) sal from emp";
-			try (Connection conn = DriverManager.getConnection(url, user, passwd);
-					Statement stmt = conn.createStatement();			
-					ResultSet rs = stmt.executeQuery(sql);) {			
-				if(rs.next() ) {				
-					do {
-						System.out.print(rs.getString("ename")+" 직원의 월급은 ");
-						System.out.print(rs.getString("sal")+"달러입니다.\n");
-					} while (rs.next());
-				} else {
-					System.out.println("추출된 행이 없습니다.");
-				}			
-			} catch (Exception e) {
-				System.err.println("오류 발생 : " + e);
-			} 
+
+		if (random) {
+			sql = "select ename, FORMAT(sal,0) sal from emp"; // 함수이름 대소문자 통일 (FROMAT, DATE_FORMAT,...)
+		} else {
+			sql = "select ename, DATE_FORMAT(hiredate, '%Y년 %m월 %d일') hiredate from emp order by hiredate";
 		}
-		else { //false
-			String sql = "SELECT ename, DATE_FORMAT(hiredate, '%Y년 %m월 %d일') hiredate from emp order by hiredate";
-			try (Connection conn = DriverManager.getConnection(url, user, passwd);
-					Statement stmt = conn.createStatement();			
-					ResultSet rs = stmt.executeQuery(sql);) {			
-				if(rs.next() ) {				
+
+		try (Connection conn = DriverManager.getConnection(url, user, passwd);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+			if (rs.next()) {
+				if (random) { // true
 					do {
-						System.out.print(rs.getString("ename")+" 직원은 ");
-						System.out.print(rs.getString("hiredate")+"에 입사하였습니다.\n");
+						System.out.print(rs.getString("ename") + " 직원의 월급은 ");
+						System.out.print(rs.getString("sal") + "달러입니다.\n");
 					} while (rs.next());
-				} else {
-					System.out.println("추출된 행이 없습니다.");
-				}			
-			} catch (Exception e) {
-				System.err.println("오류 발생 : " + e);
-			} 
+				} else { // false
+					do {
+						System.out.print(rs.getString("ename") + " 직원은 ");
+						System.out.print(rs.getString("hiredate") + "에 입사하였습니다.\n");
+					} while (rs.next());
+				}
+			} else {
+				System.out.println("추출된 행이 없습니다.");
+			}
+		} catch (Exception e) {
+			System.err.println("오류 발생 : " + e);
 		}
 	}
 }
--- JDBC/javaedu/day14/exercise/CheckEmpLab.java
+++ JDBC/javaedu/day14/exercise/CheckEmpLab.java
@@ -14,36 +14,35 @@ public class CheckEmpLab {
 		String findName = "";
 		String ctn = "";
 		Scanner sc = new Scanner(System.in);
-	
-		while(true) {
-			if(findName != "") {
-				System.out.println("계속 하시겠습니까? y/n");
-				ctn = sc.next();
-				if(ctn.equals("n") || ctn.equals("N")) {
-					break;
-				}
-			}
+
+		while (true) {
 			System.out.println("조회할 직원의 이름을 입력하세요");
 			findName = sc.next();
 
-			String sql = "SELECT ename, DATE_FORMAT(hiredate, '%Y년 %m월') hiredate, deptno from emp where ename='" + findName + "'";
+			String sql = "SELECT ename, DATE_FORMAT(hiredate, '%Y년 %m월') hiredate, deptno from emp where ename='"
+					+ findName + "'";
 			try (Connection conn = DriverManager.getConnection(url, user, passwd);
-					Statement stmt = conn.createStatement();			
-					ResultSet rs = stmt.executeQuery(sql);) {			
-				if(rs.next() ) {				
+					Statement stmt = conn.createStatement();
+					ResultSet rs = stmt.executeQuery(sql);) {
+				if (rs.next()) {
 					do {
-						System.out.print(rs.getString("ename")+" 직원은 근무중입니다.");
-						System.out.print(rs.getString("hiredate")+"에 입사했고");
-						System.out.print("현재 " + rs.getString("deptno")+"번 부서에서 근무하고 있습니다.\n");
+						System.out.print(rs.getString("ename") + " 직원은 근무중입니다.");
+						System.out.print(rs.getString("hiredate") + "에 입사했고");
+						System.out.print("현재 " + rs.getString("deptno") + "번 부서에서 근무하고 있습니다.\n");
 					} while (rs.next());
 				} else {
 					System.out.printf("%s 직원은 근무하지 않습니다.\n", findName);
-				}			
+				}
+				System.out.println("계속 하시겠습니까? y/n");
+				ctn = sc.next();
+				if (ctn.equals("n") || ctn.equals("N")) {
+					break;
+				}
 			} catch (Exception e) {
 				System.err.println("오류 발생 : " + e);
-			} 	
+			}
+
 		}
-		
 		sc.close();
 	}
 
