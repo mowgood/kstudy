a089461adfc16d991bb52b0e9c1a1c89857608e3
mowgood <99533453+mowgood@users.noreply.github.com>
2023-05-12 16:58:41 +0900

    Add Log Scheduling Filter Interceptor Error

--- Spring/springedu/src/main/java/com/example/springedu/SpringeduApplication.java
+++ Spring/springedu/src/main/java/com/example/springedu/SpringeduApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan(basePackages = {"com.example.springedu","thymeleaf.exam"})
+@ComponentScan(basePackages = {"com.example.springedu","thymeleaf.exam", "springws.exam"})
 @MapperScan(value={"com.example.springedu.dao"})
+@EnableScheduling
 public class SpringeduApplication {
 
 	public static void main(String[] args) {
