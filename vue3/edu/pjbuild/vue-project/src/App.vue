4042248181a3008668546b69ce3962162815bd61
mowgood <woforky@gmail.com>
2023-05-25 17:49:40 +0900

    Add vue project

--- vue3/edu/pjbuild/vue-project/src/App.vue
+++ vue3/edu/pjbuild/vue-project/src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="승희의 Vue 학습"/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |   
+    <router-link to="/study1">Study1</router-link> |
+    <router-link to="/work">Work</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
